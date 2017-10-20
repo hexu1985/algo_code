badee11a186ee828bc2bb646b8f16e9f142accc9
hexu <hexu_1985@sina.com>
2017-10-20 20:53:03 +0800

    add find_if

--- algorithm/include/algorithm.h
+++ algorithm/include/algorithm.h
@@ -2,14 +2,17 @@
 namespace hex {
 #include "count.h"
 #include "count_if.h"
+#include "find.h"
 #include "for_each.h"
+#include "min_element.h"
+#include "max_element.h"
 }
 #define count hex::count
 #define count_if hex::count_if
+#define find hex::find
 #define for_each hex::for_each
+#define min_element hex::min_element
+#define max_element hex::max_element
 
-#include "find.h"
-#include "min_element.h"
-#include "max_element.h"
 #include "remove.h"
 #include "remove_copy.h"
