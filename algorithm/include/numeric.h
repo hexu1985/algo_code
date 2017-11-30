ee608698f93bec3bf4f83bad2759ac36a803bc55
hexu <hexu_1985@sina.com>
2017-11-30 18:20:12 +0800

    add inner_product

--- algorithm/include/numeric.h
+++ algorithm/include/numeric.h
@@ -4,10 +4,12 @@ namespace hex {
 #include "accumulate.h"
 #include "adjacent_difference.h"
 #include "iota.h"
+#include "inner_product.h"
 
 }   // namespace hex
 
 #define accumulate hex::accumulate 
 #define adjacent_difference hex::adjacent_difference
 #define iota hex::iota
+#define inner_product hex::inner_product
 
