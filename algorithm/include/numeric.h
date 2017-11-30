edc471f1c46326fed3db2b10f560ba1c8f58d1ab
hexu <hexu_1985@sina.com>
2017-11-30 22:04:41 +0800

    add partial_sum

--- algorithm/include/numeric.h
+++ algorithm/include/numeric.h
@@ -5,6 +5,7 @@ namespace hex {
 #include "adjacent_difference.h"
 #include "iota.h"
 #include "inner_product.h"
+#include "partial_sum.h"
 
 }   // namespace hex
 
@@ -12,4 +13,5 @@ namespace hex {
 #define adjacent_difference hex::adjacent_difference
 #define iota hex::iota
 #define inner_product hex::inner_product
+#define partial_sum hex::partial_sum
 
