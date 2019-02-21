871c75d514a9226a0b4db1fda9136e681d4238a0
hexu <hexu_1985@sina.com>
2019-02-21 18:31:45 +0800

    update stopwatch11

--- stopwatch11/stopwatch11.h
+++ stopwatch11/stopwatch11.h
@@ -5,9 +5,6 @@
 	using namespace std::chrono;
 	class TimerBaseChrono {
 	public:
-		// tick type
-		typedef unsigned tick_t;
-
 		//	clears the timer
 		TimerBaseChrono() : m_start(system_clock::time_point::min()) { }
 
@@ -24,11 +21,6 @@
 		//	start the timer
 		void Start()            { m_start = std::chrono::system_clock::now(); }
 
-		// get elapsed time in ticks
-		tick_t GetTicks() {
-            return GetMs();
-		}
-
 		//	get the number of milliseconds since the timer was started
 		unsigned long GetMs() {
 			if (IsStarted()) {
@@ -38,10 +30,6 @@
 			}
 			return 0;
 		}
-
-		static unsigned GetMs(tick_t dt) {
-            return dt;
-        }
 
 	private:
 		std::chrono::system_clock::time_point m_start;
